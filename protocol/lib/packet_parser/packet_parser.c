0200218b11f5f50cab3e9df4ee6dde0af53248d4
lee-emqx <lhj@emqx.io>
2020-07-24 05:51:51 -0700

    add var packet

--- protocol/lib/packet_parser/packet_parser.c
+++ protocol/lib/packet_parser/packet_parser.c
@@ -1,34 +1,47 @@
 #include "packet_parser.h"
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 
 uint8_t packet_parse_uint8(struct mqtt_packet *packet) 
 {
     assert(packet && packet->remaining_length >= packet->pos+1);
-    return packet->payload[packet->pos++];
+    return packet->binary[packet->pos++];
 }
 
 uint16_t packet_parse_uint16(struct mqtt_packet *packet) 
 {
     assert(packet && packet->remaining_length >= packet->pos+2);
-    uint16_t res = packet->payload[packet->pos++];
-    return (res << 8) + packet->payload[packet->pos++];
+    uint16_t res = packet->binary[packet->pos++];
+    return (res << 8) + packet->binary[packet->pos++];
 }
+
+/* TODO judge endian */
+//uint16_t packet_parse_uint16(struct mqtt_packet *packet) 
+//{
+//    assert(packet && packet->remaining_length >= packet->pos+2);
+//
+//    uint16_t res = 0;
+//    memcpy((uint16_t*)&res, (uint16_t*)&(packet->binary[packet->pos]), sizeof(res));
+//    printf("res is: %#X\n", res);
+//    packet->pos+=2;
+//    return res;
+//}
 
 uint32_t packet_parse_uint32(struct mqtt_packet *packet) 
 {
     assert(packet && packet->remaining_length >= packet->pos+4);
-    uint32_t res = packet->payload[packet->pos++];
+    uint32_t res = packet->binary[packet->pos++];
 
     if (res) {
         while ((0xFF000000 & res) == 0) {
-            res = (res << 8) + packet->payload[packet->pos++];
+            res = (res << 8) + packet->binary[packet->pos++];
         }
     } else {
         int i = 4;
         while (--i > 0) {
-            res = (res << 8) + packet->payload[packet->pos++];
+            res = (res << 8) + packet->binary[packet->pos++];
         }
     }
 
@@ -38,39 +51,84 @@ uint32_t packet_parse_uint32(struct mqtt_packet *packet)
 void packet_parse_string(struct mqtt_packet *packet, char str[], uint32_t length)
 {
     assert(packet && packet->remaining_length >= packet->pos+length);
-    memcpy(str, &(packet->payload[packet->pos]), length);
+    memcpy(str, &(packet->binary[packet->pos]), length);
     packet->pos += length;
 }
 
+uint32_t packet_parse_var(struct mqtt_packet *packet) 
+{
+    assert(packet);
+    int i = 3;
+    uint32_t res = 0;
+    uint32_t multiplier = 1;
+    uint8_t byte;
+    do {
+        assert(packet->remaining_length >= packet->pos);
+
+        byte = packet->binary[packet->pos++];
+
+        res += (byte & 127) * multiplier;
+        multiplier *= 128;
+    } while (i-- && (byte & 128));
+    printf("res is %#X\n", res);
+    return res;
+
+}
 
 void packet_write_uint8(struct mqtt_packet *packet, uint8_t input)
 {
     assert(packet && packet->remaining_length >= packet->pos+1);
-    packet->payload[packet->pos++] = input;
+    packet->binary[packet->pos++] = input;
 }
 
 void packet_write_uint16(struct mqtt_packet *packet, uint16_t input)
 {
     assert(packet && packet->remaining_length >= packet->pos+2);
-    packet->payload[packet->pos++] = (input & 0xFF00) >> 8;
-    packet->payload[packet->pos++] = input & 0x00FF;
+    packet->binary[packet->pos++] = (input & 0xFF00) >> 8;
+    packet->binary[packet->pos++] = input & 0x00FF;
 }
 
 void packet_write_uint32(struct mqtt_packet *packet, uint32_t input)
 {
     assert(packet && packet->remaining_length >= packet->pos+4);
-    packet->payload[packet->pos++] = (input & 0xFF000000) >> 24;
-    packet->payload[packet->pos++] = (input & 0x00FF0000) >> 16;
-    packet->payload[packet->pos++] = (input & 0x0000FF00) >> 8;
-    packet->payload[packet->pos++] = (input & 0x000000FF);
+    packet->binary[packet->pos++] = (input & 0xFF000000) >> 24;
+    packet->binary[packet->pos++] = (input & 0x00FF0000) >> 16;
+    packet->binary[packet->pos++] = (input & 0x0000FF00) >> 8;
+    packet->binary[packet->pos++] = (input & 0x000000FF);
 }
 
 void packet_write_string(struct mqtt_packet *packet, char str[], uint32_t length) 
 {
     assert(packet && packet->remaining_length >= packet->pos+length);
-    memcpy(&(packet->payload[packet->pos]), str, length);
+    memcpy(&(packet->binary[packet->pos]), str, length);
     packet->pos += length;
 }
+ 
+void packet_write_var(struct mqtt_packet *packet, uint32_t input)
+{
+    assert(packet);
+    uint8_t byte;
+    do {
+        assert(packet->remaining_length >= packet->pos);
+        /* Can be optimized */
+        byte = input % 128;
+        input /= 128;
+        if (input > 0) {
+            byte |= 128;
+        }
+        packet->binary[packet->pos++] = byte;
+        
+
+    } while (input);
+}
+
+
+
+
+
+        
+
+
 
 
 
--- protocol/lib/packet_parser/packet_parser.h
+++ protocol/lib/packet_parser/packet_parser.h
@@ -11,34 +11,25 @@ typedef unsigned int uint32_t;
 typedef signed char int8_t;
 
 
-struct header{
-    
-}
-
 
 struct mqtt_packet {
     uint8_t *binary;
     uint32_t pos;
-    uint32_t len;
+    uint32_t remaining_length;
 };
 
-struct mqtt_packet a;
-a.binary = variable;
-a.pos = 0;
-a.len = 20;
-
-string t = paclet_parse_uint8(&packet);
-int y = packet_parse_uint8(&packet);
 
 uint8_t packet_parse_uint8(struct mqtt_packet *packet); 
 uint16_t packet_parse_uint16(struct mqtt_packet *packet); 
 uint32_t packet_parse_uint32(struct mqtt_packet *packet); 
 void packet_parse_string(struct mqtt_packet *packet, char str[], uint32_t length);
+uint32_t packet_parse_var(struct mqtt_packet *packet);
 
 
 void packet_write_uint8(struct mqtt_packet *packet, uint8_t input);
 void packet_write_uint16(struct mqtt_packet *packet, uint16_t input);
 void packet_write_uint32(struct mqtt_packet * packet, uint32_t intput);
 void packet_write_string(struct mqtt_packet *packet, char str[], uint32_t length);
+void packet_write_var(struct mqtt_packet *packet, uint32_t);
 
 #endif
