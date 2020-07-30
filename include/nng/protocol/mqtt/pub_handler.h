de7373edb330bdaeed6c0bf0ac20530878f119b4
alvin1221 <lzzxhhh@126.com>
2020-07-30 16:28:28 +0800

    Fix decode_pub_message

--- include/nng/protocol/mqtt/pub_handler.h
+++ include/nng/protocol/mqtt/pub_handler.h
@@ -10,183 +10,202 @@
 typedef uint32_t variable_integer;
 
 struct variable_string {
-    uint32_t str_len;
-    char *str_body;
+	uint32_t str_len;
+	char *str_body;
 };
 
-struct variable_binary{
-    uint32_t data_len;
-    uint8_t *data;
+struct variable_binary {
+	uint32_t data_len;
+	uint8_t *data;
 };
 
 //MQTT Control Packet types
 typedef enum {
-    RESERVED = 0,
-    CONNECT = 1,
-    CONNACK = 2,
-    PUBLISH = 3,
-    PUBACK = 4,
-    PUBREC = 5,
-    PUBREL = 6,
-    PUBCOMP = 7,
-    SUBSCRIBE = 8,
-    SUBACK = 9,
-    UNSUBSCRIBE = 10,
-    UNSUBACK = 11,
-    PINGREQ = 12,
-    PINGRESP = 13,
-    DISCONNECT = 14,
-    AUTH = 15
+	RESERVED = 0,
+	CONNECT = 1,
+	CONNACK = 2,
+	PUBLISH = 3,
+	PUBACK = 4,
+	PUBREC = 5,
+	PUBREL = 6,
+	PUBCOMP = 7,
+	SUBSCRIBE = 8,
+	SUBACK = 9,
+	UNSUBSCRIBE = 10,
+	UNSUBACK = 11,
+	PINGREQ = 12,
+	PINGRESP = 13,
+	DISCONNECT = 14,
+	AUTH = 15
 } mqtt_control_packet_types;
 
 
 //MQTT Fixed header
 struct fixed_header {
-    //flag_bits
-    union {
-        uint8_t bit0: 1;
-        uint8_t retain: 1;
-    };
-    union {
-        uint8_t bit1: 1;
-        uint8_t bit2: 1;
-        uint8_t qos: 2;
-    };
-    union {
-        uint8_t bit3: 1;
-        uint8_t dup: 1;
-    };
-    //packet_types
-    mqtt_control_packet_types packet_type: 4;
-    //remaining length
-    uint32_t remain_len;
+	//flag_bits
+	union {
+		uint8_t bit0: 1;
+		uint8_t retain: 1;
+	};
+	union {
+		uint8_t bit1: 1;
+		uint8_t bit2: 1;
+		uint8_t qos: 2;
+	};
+	union {
+		uint8_t bit3: 1;
+		uint8_t dup: 1;
+	};
+	//packet_types
+	mqtt_control_packet_types packet_type: 4;
+	//remaining length
+	uint32_t remain_len;
 };
 
 typedef enum {
-    PAYLOAD_FORMAT_INDICATOR = 1,
-    MESSAGE_EXPIRY_INTERVAL = 2,
-    CONTENT_TYPE = 3,
-    RESPONSE_TOPIC = 8,
-    CORRELATION_DATA = 9,
-    SUBSCRIPTION_IDENTIFIER = 11,
-    SESSION_EXPIRY_INTERVAL = 17,
-    ASSIGNED_CLIENT_IDENTIFIER = 18,
-    SERVER_KEEP_ALIVE = 19,
-    AUTHENTICATION_METHOD = 21,
-    AUTHENTICATION_DATA = 22,
-    REQUEST_PROBLEM_INFORMATION = 23,
-    WILL_DELAY_INTERVAL = 24,
-    REQUEST_RESPONSE_INFORMATION = 25,
-    RESPONSE_INFORMATION = 26,
-    SERVER_REFERENCE = 28,
-    REASON_STRING = 31,
-    RECEIVE_MAXIMUM = 33,
-    TOPIC_ALIAS_MAXIMUM = 34,
-    TOPIC_ALIAS = 35,
-    PUBLISH_MAXIMUM_QOS = 36,
-    RETAIN_AVAILABLE = 37,
-    USER_PROPERTY = 38,
-    MAXIMUM_PACKET_SIZE = 39,
-    WILDCARD_SUBSCRIPTION_AVAILABLE = 40,
-    SUBSCRIPTION_IDENTIFIER_AVAILABLE = 41,
-    SHARED_SUBSCRIPTION_AVAILABLE = 42
+	SUCCESS = 0,
+	NORMAL_DISCONNECTION = 0,
+	GRANTED_QOS_0 = 0,
+	GRANTED_QOS_1 = 1,
+	GRANTED_QOS_2 = 2,
+	DISCONNECT_WITH_WILL_MESSAGE = 4,
+	NO_MATCHING_SUBSCRIBERS = 16,
+	NO_SUBSCRIPTION_EXISTED = 17,
+	CONTINUE_AUTHENTICATION = 24,
+	RE_AUTHENTICATE = 25,
+	UNSPECIFIED_ERROR = 128,
+	MALFORMED_PACKET = 129,
+	PROTOCOL_ERROR = 130,
+	IMPLEMENTATION_SPECIFIC_ERROR = 131,
+	UNSUPPORTED_PROTOCOL_VERSION = 132,
+	CLIENT_IDENTIFIER_NOT_VALID = 133,
+	BAD_USER_NAME_OR_PASSWORD = 134,
+	NOT_AUTHORIZED = 135,
+	SERVER_UNAVAILABLE = 136,
+	SERVER_BUSY = 137,
+	BANNED = 138,
+	SERVER_SHUTTING_DOWN = 139,
+	BAD_AUTHENTICATION_METHOD = 140,
+	KEEP_ALIVE_TIMEOUT = 141,
+	SESSION_TAKEN_OVER = 142,
+	TOPIC_FILTER_INVALID = 143,
+	TOPIC_NAME_INVALID = 144,
+	PACKET_IDENTIFIER_IN_USE = 145,
+	PACKET_IDENTIFIER_NOT_FOUND = 146,
+	RECEIVE_MAXIMUM_EXCEEDED = 147,
+	TOPIC_ALIAS_INVALID = 148,
+	PACKET_TOO_LARGE = 149,
+	MESSAGE_RATE_TOO_HIGH = 150,
+	QUOTA_EXCEEDED = 151,
+	ADMINISTRATIVE_ACTION = 152,
+	PAYLOAD_FORMAT_INVALID = 153,
+	RETAIN_NOT_SUPPORTED = 154,
+	QOS_NOT_SUPPORTED = 155,
+	USE_ANOTHER_SERVER = 156,
+	SERVER_MOVED = 157,
+	SHARED_SUBSCRIPTIONS_NOT_SUPPORTED = 158,
+	CONNECTION_RATE_EXCEEDED = 159,
+	MAXIMUM_CONNECT_TIME = 160,
+	SUBSCRIPTION_IDENTIFIERS_NOT_SUPPORTED = 161,
+	WILDCARD_SUBSCRIPTIONS_NOT_SUPPORTED = 162
+
+} reason_code;
+
+typedef enum {
+	PAYLOAD_FORMAT_INDICATOR = 1,
+	MESSAGE_EXPIRY_INTERVAL = 2,
+	CONTENT_TYPE = 3,
+	RESPONSE_TOPIC = 8,
+	CORRELATION_DATA = 9,
+	SUBSCRIPTION_IDENTIFIER = 11,
+	SESSION_EXPIRY_INTERVAL = 17,
+	ASSIGNED_CLIENT_IDENTIFIER = 18,
+	SERVER_KEEP_ALIVE = 19,
+	AUTHENTICATION_METHOD = 21,
+	AUTHENTICATION_DATA = 22,
+	REQUEST_PROBLEM_INFORMATION = 23,
+	WILL_DELAY_INTERVAL = 24,
+	REQUEST_RESPONSE_INFORMATION = 25,
+	RESPONSE_INFORMATION = 26,
+	SERVER_REFERENCE = 28,
+	REASON_STRING = 31,
+	RECEIVE_MAXIMUM = 33,
+	TOPIC_ALIAS_MAXIMUM = 34,
+	TOPIC_ALIAS = 35,
+	PUBLISH_MAXIMUM_QOS = 36,
+	RETAIN_AVAILABLE = 37,
+	USER_PROPERTY = 38,
+	MAXIMUM_PACKET_SIZE = 39,
+	WILDCARD_SUBSCRIPTION_AVAILABLE = 40,
+	SUBSCRIPTION_IDENTIFIER_AVAILABLE = 41,
+	SHARED_SUBSCRIPTION_AVAILABLE = 42
 } properties_type;
 
 struct property {
-    properties_type type: 32;
-    uint32_t len;
-    void *data; //Dynamic alloc memory, need to call the function "nng_free(void *buf, size_t sz)" to free;
+	properties_type type: 32;
+	uint32_t len;
+	void *data; //Dynamic alloc memory, need to call the function "nng_free(void *buf, size_t sz)" to free;
 };
 
 //Special for publish message data structure
-struct property_content{
-    uint8_t payload_fmt_indicator;
-    uint32_t msg_expiry_interval;
-    uint16_t topic_alias;
-    struct variable_string response_topic;
-    struct variable_binary correlation_data;
-    struct variable_string user_property;
-    variable_integer subscription_identifier;
-    struct variable_string content_type;
+struct property_content {
+	uint8_t payload_fmt_indicator;
+	uint32_t msg_expiry_interval;
+	uint16_t topic_alias;
+	struct variable_string response_topic;
+	struct variable_binary correlation_data;
+	struct variable_string user_property;
+	variable_integer subscription_identifier;
+	struct variable_string content_type;
 };
 
-#define PUBLISH_PROPERTIES_TOTAL 10
+//#define PUBLISH_PROPERTIES_TOTAL 10
 
 //Properties
 struct properties {
-    uint32_t len; //property length, exclude itself,variable byte integer;
+	uint32_t len; //property length, exclude itself,variable byte integer;
 //    struct property prop_content[PUBLISH_PROPERTIES_TOTAL];
-    struct property_content content;
+	struct property_content content;
 };
-
-
 
 
 //MQTT Variable header
 struct variable_header {
-    uint16_t packet_identifier;
-    struct variable_string topic_name;
-    struct properties properties;
+	union {
+		uint16_t packet_identifier;
+		struct variable_string topic_name;
+		struct properties properties;
+	} publish;
+
+	union {
+		uint16_t packet_identifier;
+		reason_code reason_code;
+		struct properties properties;
+	} puback;
+
+	union {
+
+	}pubrec;
+
+	union {
+
+	}pubrel;
+
+	union {
+
+	} pubcomp ;
 };
 
-typedef enum {
-    SUCCESS = 0,
-    NORMAL_DISCONNECTION = 0,
-    GRANTED_QOS_0 = 0,
-    GRANTED_QOS_1 = 1,
-    GRANTED_QOS_2 = 2,
-    DISCONNECT_WITH_WILL_MESSAGE = 4,
-    NO_MATCHING_SUBSCRIBERS = 16,
-    NO_SUBSCRIPTION_EXISTED = 17,
-    CONTINUE_AUTHENTICATION = 24,
-    RE_AUTHENTICATE = 25,
-    UNSPECIFIED_ERROR = 128,
-    MALFORMED_PACKET = 129,
-    PROTOCOL_ERROR = 130,
-    IMPLEMENTATION_SPECIFIC_ERROR = 131,
-    UNSUPPORTED_PROTOCOL_VERSION = 132,
-    CLIENT_IDENTIFIER_NOT_VALID = 133,
-    BAD_USER_NAME_OR_PASSWORD = 134,
-    NOT_AUTHORIZED = 135,
-    SERVER_UNAVAILABLE = 136,
-    SERVER_BUSY = 137,
-    BANNED = 138,
-    SERVER_SHUTTING_DOWN = 139,
-    BAD_AUTHENTICATION_METHOD = 140,
-    KEEP_ALIVE_TIMEOUT = 141,
-    SESSION_TAKEN_OVER = 142,
-    TOPIC_FILTER_INVALID = 143,
-    TOPIC_NAME_INVALID = 144,
-    PACKET_IDENTIFIER_IN_USE = 145,
-    PACKET_IDENTIFIER_NOT_FOUND = 146,
-    RECEIVE_MAXIMUM_EXCEEDED = 147,
-    TOPIC_ALIAS_INVALID = 148,
-    PACKET_TOO_LARGE = 149,
-    MESSAGE_RATE_TOO_HIGH = 150,
-    QUOTA_EXCEEDED = 151,
-    ADMINISTRATIVE_ACTION = 152,
-    PAYLOAD_FORMAT_INVALID = 153,
-    RETAIN_NOT_SUPPORTED = 154,
-    QOS_NOT_SUPPORTED = 155,
-    USE_ANOTHER_SERVER = 156,
-    SERVER_MOVED = 157,
-    SHARED_SUBSCRIPTIONS_NOT_SUPPORTED = 158,
-    CONNECTION_RATE_EXCEEDED = 159,
-    MAXIMUM_CONNECT_TIME = 160,
-    SUBSCRIPTION_IDENTIFIERS_NOT_SUPPORTED = 161,
-    WILDCARD_SUBSCRIPTIONS_NOT_SUPPORTED = 162
-
-} reason_code;
 
 struct mqtt_payload {
-    uint8_t *payload;
-    uint32_t payload_len;
+	uint8_t *payload;
+	uint32_t payload_len;
 };
 
 struct pub_packet_struct {
-    struct fixed_header fixed_header;
-    struct variable_header variable_header;
-    struct mqtt_payload payload_body;
+	struct fixed_header fixed_header;
+	struct variable_header variable_header;
+	struct mqtt_payload payload_body;
 
 };
 
--- src/protocol/mqtt/pub_handler.c
+++ src/protocol/mqtt/pub_handler.c
@@ -19,11 +19,13 @@ static int32_t get_utf8_str(char *dest, const uint8_t *src, int *pos);
 
 static int utf8_check(const char *str, size_t length);
 
+#if 0
 uint32_t get_uint16(const uint8_t *buf);
 
 uint32_t get_uint32(const uint8_t *buf);
 
 uint64_t get_uint64(const uint8_t *buf);
+#endif
 
 uint16_t get_variable_binary(uint8_t *dest, const uint8_t *src);
 
@@ -36,150 +38,177 @@ uint16_t get_variable_binary(uint8_t *dest, const uint8_t *src);
  */
 void pub_handler(nng_msg *msg)
 {
-    struct pub_packet_struct pub_packet;
+	struct pub_packet_struct pub_packet;
 
-    if (decode_pub_message(msg, &pub_packet)) {
-        //TODO save some useful publish message info and properties to global mqtt context while decode succeed
-        //TODO search clients' pipe from topic_name
-        //TODO do publish actions, eq: send payload to clients dependent on QoS ,topic alias if exists
-    }
+	if (decode_pub_message(msg, &pub_packet)) {
+		//TODO save some useful publish message info and properties to global mqtt context while decode succeed
+		//TODO search clients' pipe from topic_name
+		//TODO do publish actions, eq: send payload to clients dependent on QoS ,topic alias if exists
+	}
 }
 
 static bool encode_pub_message(uint8_t *dest, struct pub_packet_struct *pub_packet)
 {
 
-    return true;
+	return true;
 }
 
 static bool decode_pub_message(nng_msg *msg, struct pub_packet_struct *pub_packet)
 {
-    uint8_t *msg_body = nng_msg_body(msg);
-    size_t msg_len = nng_msg_len(msg);
+	uint8_t *msg_body = nng_msg_body(msg);
+	size_t msg_len = nng_msg_len(msg);
 
-    int pos = 0;
-    int temp_pos = 0;
+	int pos = 0;
+	int temp_pos = 0;
+	int len;
 
-    memcpy((uint8_t *) &pub_packet->fixed_header, msg_body, 1);
-    ++pos;
-    pub_packet->fixed_header.remain_len = get_var_integer(msg_body, &pos);
+	memcpy((uint8_t *) &pub_packet->fixed_header, msg_body, 1);
+	++pos;
+	pub_packet->fixed_header.remain_len = get_var_integer(msg_body, &pos);
 
-    if (pub_packet->fixed_header.remain_len <= msg_len - pos) {
+	if (pub_packet->fixed_header.remain_len <= msg_len - pos) {
 
-        if (pub_packet->fixed_header.packet_type == PUBLISH) {
+		switch (pub_packet->fixed_header.packet_type) {
+			case PUBLISH:
+				//variable header
+				//topic length
+				len = get_utf8_str(pub_packet->variable_header.publish.topic_name.str_body, msg_body + pos, &pos);
+				if (len < 0 ||
+				    strstr(pub_packet->variable_header.publish.topic_name.str_body, "+") ||
+				    strstr(pub_packet->variable_header.publish.topic_name.str_body, "#")) {
+					//protocol error
+					return false;
+				}
+				pub_packet->variable_header.publish.topic_name.str_len = len;
 
-            //variable header
-            //TODO deal with topic alias
-            int topic_len = get_utf8_str(pub_packet->variable_header.topic_name.str_body, msg_body + pos, &pos);
-            if (topic_len < 0 ||
-                strstr(pub_packet->variable_header.topic_name.str_body, "+") ||
-                strstr(pub_packet->variable_header.topic_name.str_body, "#")) {
-                //protocol error
-                return false;
-            }
-            pub_packet->variable_header.topic_name.str_len = topic_len;
+				if (pub_packet->fixed_header.qos > 0) { //extract packet_identifier while qos > 0
+					NNI_GET16(msg_body + pos, pub_packet->variable_header.publish.packet_identifier);
+					pos += 2;
+				}
 
-            if (pub_packet->fixed_header.qos > 0) { //extract packet_identifier while qos > 0
-                pub_packet->variable_header.packet_identifier = get_uint16(msg_body + pos);
-                pos += 2;
-            }
+				pub_packet->variable_header.publish.properties.len = get_var_integer(msg_body, &pos);
+				int used_pos = pos;
+				if (pub_packet->variable_header.publish.properties.len > 0) {
+					for (uint32_t i = 0; i < pub_packet->variable_header.publish.properties.len;) {
+						properties_type prop_type = get_var_integer(msg_body, &pos);
+						switch (prop_type) {
+							case PAYLOAD_FORMAT_INDICATOR:
+								pub_packet->variable_header.publish.properties.content.payload_fmt_indicator = *(
+										msg_body +
+										pos);
+								++pos;
+								++i;
+								break;
 
-            pub_packet->variable_header.properties.len = get_var_integer(msg_body, &pos);
-            int used_pos = pos;
-            if (pub_packet->variable_header.properties.len > 0) {
-                for (uint32_t i = 0; i < pub_packet->variable_header.properties.len;) {
-                    properties_type prop_type = get_var_integer(msg_body, &pos);
-                    switch (prop_type) {
-                        case PAYLOAD_FORMAT_INDICATOR:
-                            pub_packet->variable_header.properties.content.payload_fmt_indicator = *(msg_body + pos);
-                            ++pos;
-                            ++i;
-                            break;
+							case MESSAGE_EXPIRY_INTERVAL:
+								NNI_GET32(
+										msg_body + pos,
+										pub_packet->variable_header.publish.properties.content.msg_expiry_interval);
+								pos += 4;
+								i += 4;
+								break;
 
-                        case MESSAGE_EXPIRY_INTERVAL:
-                            pub_packet->variable_header.properties.content.msg_expiry_interval = get_uint32(
-                                    msg_body + pos);
-                            pos += 4;
-                            i += 4;
-                            break;
+							case CONTENT_TYPE:
+								pub_packet->variable_header.publish.properties.content.content_type.str_len = get_utf8_str(
+										pub_packet->variable_header.publish.properties.content.content_type.str_body,
+										msg_body,
+										&pos);
+								i = i + pub_packet->variable_header.publish.properties.content.content_type.str_len + 2;
+								break;
 
-                        case CONTENT_TYPE:
-                            pub_packet->variable_header.properties.content.content_type.str_len = get_utf8_str(
-                                    pub_packet->variable_header.properties.content.content_type.str_body, msg_body,
-                                    &pos);
-                            i = i + pub_packet->variable_header.properties.content.content_type.str_len + 2;
-                            break;
+							case TOPIC_ALIAS:
+								NNI_GET16(
+										msg_body + pos,
+										pub_packet->variable_header.publish.properties.content.topic_alias);
+								pos += 2;
+								i += 2;
 
-                        case TOPIC_ALIAS:
-                            pub_packet->variable_header.properties.content.topic_alias = get_uint16(msg_body + pos);
-                            pos += 2;
-                            i += 2;
-
-                            if (pub_packet->variable_header.properties.content.topic_alias == 0) {
-                                //TODO protocol error, free memory and return ?
+								if (pub_packet->variable_header.publish.properties.content.topic_alias == 0) {
+									//TODO protocol error, free memory and return ?
 //                                return false;
-                            }
+								}
 
-                            break;
+								break;
 
-                        case RESPONSE_TOPIC:
-                            pub_packet->variable_header.properties.content.response_topic.str_len = get_utf8_str(
-                                    pub_packet->variable_header.properties.content.response_topic.str_body, msg_body,
-                                    &pos);
-                            i = i + pub_packet->variable_header.properties.content.content_type.str_len + 2;
+							case RESPONSE_TOPIC:
+								pub_packet->variable_header.publish.properties.content.response_topic.str_len = get_utf8_str(
+										pub_packet->variable_header.publish.properties.content.response_topic.str_body,
+										msg_body,
+										&pos);
+								i = i + pub_packet->variable_header.publish.properties.content.content_type.str_len + 2;
+								break;
 
-                            break;
+							case CORRELATION_DATA:
+								pub_packet->variable_header.publish.properties.content.correlation_data.data_len = get_variable_binary(
+										pub_packet->variable_header.publish.properties.content.correlation_data.data,
+										msg_body + pos);
+								pos += pub_packet->variable_header.publish.properties.content.correlation_data.data_len +
+								       2;
+								i += pub_packet->variable_header.publish.properties.content.correlation_data.data_len +
+								     2;
+								break;
 
-                        case CORRELATION_DATA:
-                            pub_packet->variable_header.properties.content.correlation_data.data_len = get_variable_binary(
-                                    pub_packet->variable_header.properties.content.correlation_data.data,
-                                    msg_body + pos);
-                            pos += pub_packet->variable_header.properties.content.correlation_data.data_len + 2;
-                            i += pub_packet->variable_header.properties.content.correlation_data.data_len + 2;
+							case USER_PROPERTY:
+								pub_packet->variable_header.publish.properties.content.response_topic.str_len = get_utf8_str(
+										pub_packet->variable_header.publish.properties.content.user_property.str_body,
+										msg_body,
+										&pos);
+								i = i + pub_packet->variable_header.publish.properties.content.user_property.str_len +
+								    2;
+								break;
 
-                            break;
+							case SUBSCRIPTION_IDENTIFIER:
+								temp_pos = pos;
+								pub_packet->variable_header.publish.properties.content.subscription_identifier = get_var_integer(
+										msg_body, &pos);
+								i += (pos - temp_pos);
+								break;
 
-                        case USER_PROPERTY:
-                            pub_packet->variable_header.properties.content.response_topic.str_len = get_utf8_str(
-                                    pub_packet->variable_header.properties.content.user_property.str_body, msg_body,
-                                    &pos);
-                            i = i + pub_packet->variable_header.properties.content.user_property.str_len + 2;
-                            break;
+							default:
+								i++;
+								break;
+						}
+					}
+				}
 
-                        case SUBSCRIPTION_IDENTIFIER:
-                            temp_pos = pos;
-                            pub_packet->variable_header.properties.content.subscription_identifier = get_var_integer(
-                                    msg_body, &pos);
-                            i += (pos - temp_pos);
-                            break;
+				//payload
+				pub_packet->payload_body.payload_len = msg_len - used_pos;
+//				pub_packet->payload_body.payload = nni_alloc(pub_packet->payload_body.payload_len);
+				pub_packet->payload_body.payload = msg_body + pos;
+//				memcpy(pub_packet->payload_body.payload, msg_body + pos, pub_packet->payload_body.payload_len);
+				break;
 
-                        default:
-                            i++;
-                            break;
-                    }
-                }
-            }
+			case PUBACK:
+				NNI_GET16(msg_body + pos, pub_packet->variable_header.puback.packet_identifier);
+				pos += 2;
+				if (pub_packet->fixed_header.remain_len == 2) {
+					//means reason code is success 0x00
+					break;
+				}
+				pub_packet->variable_header.puback.reason_code = *(msg_body + pos);
+				++pos;
+				break;
 
-            //payload
-            pub_packet->payload_body.payload_len = msg_len - used_pos;
-            pub_packet->payload_body.payload = nni_alloc(pub_packet->payload_body.payload_len);
-            memcpy(pub_packet->payload_body.payload, msg_body + pos, pub_packet->payload_body.payload_len);
+			case PUBREC:
+				break;
 
-            return true;
-        } else if (pub_packet->fixed_header.packet_type == PUBACK) {
-            //TODO handle PUCACK
+			case PUBREL:
+				break;
 
-        } else if (pub_packet->fixed_header.packet_type == PUBREC) {
+			case PUBCOMP:
+				break;
 
-        } else if (pub_packet->fixed_header.packet_type == PUBREL) {
+			default:
+				break;
+		}
+		return true;
 
-        } else if (pub_packet->fixed_header.packet_type == PUBCOMP) {
-
-        }
-    }
+	}
 
 
-    return false;
+	return false;
 }
+
 
 /**
  * Get variable integer value
@@ -190,53 +219,57 @@ static bool decode_pub_message(nng_msg *msg, struct pub_packet_struct *pub_packe
  */
 uint32_t get_var_integer(const uint8_t *buf, int *pos)
 {
-    uint8_t temp;
-    uint32_t result = 0;
-    uint8_t loop_times = 4;
-    int p = *pos;
+	uint8_t temp;
+	uint32_t result = 0;
+	uint8_t loop_times = 4;
+	int p = *pos;
 
-    do {
-        temp = *(buf + (p++));
-        result = (temp & 0x7F) | (result * 128);
-    }
-    while ((temp & 0x80) && --loop_times);
-    *pos = p;
-    return result;
+	do {
+		temp = *(buf + (p++));
+		result = (temp & 0x7F) | (result * 128);
+	}
+	while ((temp & 0x80) && --loop_times);
+	*pos = p;
+	return result;
 }
 
+#if 0
 uint32_t get_uint32(const uint8_t *buf)
 {
-    return (((uint32_t) ((uint8_t) (buf)[0])) >> 24u) +
-           (((uint32_t) ((uint8_t) (buf)[+1])) >> 16u) +
-           (((uint32_t) ((uint8_t) (buf)[+2])) >> 8u) +
-           (((uint32_t) (uint8_t) (buf)[+3]));
+	return (((uint32_t) ((uint8_t) (buf)[0])) >> 24u) +
+		   (((uint32_t) ((uint8_t) (buf)[+1])) >> 16u) +
+		   (((uint32_t) ((uint8_t) (buf)[+2])) >> 8u) +
+		   (((uint32_t) (uint8_t) (buf)[+3]));
 }
 
 uint64_t get_uint64(const uint8_t *buf)
 {
-    return (((uint64_t) ((uint8_t) (buf)[0])) >> 56u) +
-           (((uint64_t) ((uint8_t) (buf)[1])) >> 48u) +
-           (((uint64_t) ((uint8_t) (buf)[2])) >> 40u) +
-           (((uint64_t) ((uint8_t) (buf)[3])) >> 32u) +
-           (((uint64_t) ((uint8_t) (buf)[4])) >> 24u) +
-           (((uint64_t) ((uint8_t) (buf)[5])) >> 16u) +
-           (((uint64_t) ((uint8_t) (buf)[6])) >> 8u) +
-           (((uint64_t) (uint8_t) (buf)[7]));
+	return (((uint64_t) ((uint8_t) (buf)[0])) >> 56u) +
+		   (((uint64_t) ((uint8_t) (buf)[1])) >> 48u) +
+		   (((uint64_t) ((uint8_t) (buf)[2])) >> 40u) +
+		   (((uint64_t) ((uint8_t) (buf)[3])) >> 32u) +
+		   (((uint64_t) ((uint8_t) (buf)[4])) >> 24u) +
+		   (((uint64_t) ((uint8_t) (buf)[5])) >> 16u) +
+		   (((uint64_t) ((uint8_t) (buf)[6])) >> 8u) +
+		   (((uint64_t) (uint8_t) (buf)[7]));
 }
 
 uint32_t get_uint16(const uint8_t *buf)
 {
-    return (((uint32_t) ((uint8_t) (buf)[0])) >> 8u) +
-           (((uint32_t) (uint8_t) (buf)[1]));
+	return (((uint32_t) ((uint8_t) (buf)[0])) >> 8u) +
+		   (((uint32_t) (uint8_t) (buf)[1]));
 }
+#endif
 
 uint16_t get_variable_binary(uint8_t *dest, const uint8_t *src)
 {
-    uint16_t len = get_uint16(src);
-    dest = nni_alloc(len);
+//	uint16_t len = get_uint16(src);
+	uint16_t len = 0;
+	NNI_GET16(src, len);
+	dest = nni_alloc(len);
 
-    memcpy(dest, src + 2, len);
-    return len;
+	memcpy(dest, src + 2, len);
+	return len;
 }
 
 /**
@@ -249,50 +282,51 @@ uint16_t get_variable_binary(uint8_t *dest, const uint8_t *src)
  */
 static int32_t get_utf8_str(char *dest, const uint8_t *src, int *pos)
 {
-    int32_t str_len = get_uint16(src + *pos);
-    dest = nng_alloc(str_len); //Do not forget to free
-    *pos = (*pos) + 2;
-    if (str_len > 0) {
-        if (utf8_check((const char *) (src + *pos), str_len)) {
-            strncpy(dest, (char *) (src + *pos), str_len);
-            *pos = (*pos) + str_len;
-        } else {
-            str_len = -1;
-        }
-    }
-    return str_len;
+	int32_t str_len = 0;
+	NNI_GET16(src + (*pos), str_len);
+	dest = nng_alloc(str_len); //Do not forget to free
+	*pos = (*pos) + 2;
+	if (str_len > 0) {
+		if (utf8_check((const char *) (src + *pos), str_len)) {
+			strncpy(dest, (char *) (src + *pos), str_len);
+			*pos = (*pos) + str_len;
+		} else {
+			str_len = -1;
+		}
+	}
+	return str_len;
 }
 
 static int utf8_check(const char *str, size_t length)
 {
-    size_t i = 0;
-    int nBytes = 0;////UTF8可用1-6个字节编码,ASCII用一个字节
-    unsigned char ch = 0;
-    bool bAllAscii = true;//如果全部都是ASCII,说明不是UTF-8
-    while (i < length) {
-        ch = *(str + i);
-        if ((ch & 0x80) != 0)
-            bAllAscii = false;
-        if (nBytes == 0) {
-            if ((ch & 0x80) != 0) {
-                while ((ch & 0x80) != 0) {
-                    ch <<= 1;
-                    nBytes++;
-                }
-                if ((nBytes < 2) || (nBytes > 6)) {
-                    return 0;
-                }
-                nBytes--;
-            }
-        } else {
-            if ((ch & 0xc0) != 0x80) {
-                return 0;
-            }
-            nBytes--;
-        }
-        i++;
-    }
-    if (bAllAscii)
-        return false;
-    return (nBytes == 0);
+	size_t i = 0;
+	int nBytes = 0;////UTF8可用1-6个字节编码,ASCII用一个字节
+	unsigned char ch = 0;
+	bool bAllAscii = true;//如果全部都是ASCII,说明不是UTF-8
+	while (i < length) {
+		ch = *(str + i);
+		if ((ch & 0x80) != 0)
+			bAllAscii = false;
+		if (nBytes == 0) {
+			if ((ch & 0x80) != 0) {
+				while ((ch & 0x80) != 0) {
+					ch <<= 1;
+					nBytes++;
+				}
+				if ((nBytes < 2) || (nBytes > 6)) {
+					return 0;
+				}
+				nBytes--;
+			}
+		} else {
+			if ((ch & 0xc0) != 0x80) {
+				return 0;
+			}
+			nBytes--;
+		}
+		i++;
+	}
+	if (bAllAscii)
+		return false;
+	return (nBytes == 0);
 }
