9df3662f5d88aec6b4b6b8ee12ea9950096ba7f1
lee-emqx <lhj@emqx.io>
2020-08-10 15:44:20 +0800

    * NEW [nanolib] add print_db_tree for test, searce_client for pub

--- emq/nanolib/mqtt_db.c
+++ emq/nanolib/mqtt_db.c
@@ -11,6 +11,7 @@
 /* TODO */
 void create_db_tree(struct db_tree **db)
 {
+	debug("CREATE_DB_TREE_START");
 	*db = (struct db_tree *)zmalloc(sizeof(struct db_tree)); 
 	memset(*db, 0, sizeof(struct db_tree));
 	struct db_node *node = (struct db_node *)zmalloc(sizeof(struct db_node));
@@ -18,7 +19,45 @@ void create_db_tree(struct db_tree **db)
     node->topic = (char*)zmalloc(sizeof(char)*2);
 	memcpy(&node->topic, "\0", sizeof(char)*2);
 	(*db)->root = node;
+	debug("CREATE_DB_TREE_END");
 
+}
+
+
+void destory_db_tree(struct db_tree *db)
+{
+	debug("DESTORY_DB_TREE_START");
+	/* TODO */
+
+	debug("DESTORY_DB_TREE_END");
+}
+
+void print_db_tree(struct db_tree *db) 
+{
+	debug("PRINT_DB_TREE_START");
+	/* TODO */
+	assert(db);
+	struct db_node *node = db->root;
+	puts("-----------DB_TREE-----------");
+	while (node) {
+		struct db_node *tmp = node;
+		while (tmp) {
+			printf("%s\n", tmp->topic);
+			tmp = tmp->next;
+		}
+		node = node->down;
+		if (node) {
+			puts("-----------------------------");
+		}
+	}
+// 	for (; node; node = node->down) {
+// 		for (struct db_node *mnode = node; mnode; mnode = mnode->next) {
+//  			printf("%s\n", mnode->topic);
+// 		}
+// 	}
+
+	puts("-----------DB_TREE-----------");
+	debug("PRINT_DB_TREE_END");
 }
 
 struct db_node *new_db_node(char *topic)
@@ -45,6 +84,8 @@ void delete_db_node(struct db_node *node)
 /* Add node when sub do not find a node on the tree */
 void add_node(struct topic_and_node *input, struct client *id)
 {
+	/* TODO */
+	/* fixed add NULL at pointer end */
     assert(input && id);
     struct db_node *tmp_node = NULL;
     struct db_node *new_node = NULL;
@@ -54,6 +95,8 @@ void add_node(struct topic_and_node *input, struct client *id)
         input->node->down = new_db_node(*(++topic_queue));
         input->node->down->up = input->node;
         new_node = input->node->down;
+		new_node->down = NULL;
+		new_node->next = NULL;
 
     } else {
 		new_node = new_db_node(*topic_queue);
@@ -73,12 +116,15 @@ void add_node(struct topic_and_node *input, struct client *id)
         new_node->down = new_db_node(*topic_queue);
         new_node->down->up = new_node;
         new_node = new_node->down;
+		new_node->down = NULL;
+		new_node->next = NULL;
     }
     new_node->sub_client = (struct client*)zmalloc(sizeof(struct client));
     memcpy(new_node->sub_client, id, sizeof(struct client));
     return;
 }
-
+/*	For duplicate node 
+	TODO*/
 void del_node(struct db_node *node) 
 {
     assert(node);
@@ -157,8 +203,6 @@ void del_client(struct topic_and_node *input, char *id)
                 before_client->next = before_client->next->next;
 				delete_client(client);
             } else {
-				// puts("del_client");
-				// puts(input->node->sub_client->id);
 				before_client = input->node->sub_client; 
 				if (input->node->sub_client->next) {
 					input->node->sub_client = input->node->sub_client->next;
@@ -167,13 +211,6 @@ void del_client(struct topic_and_node *input, char *id)
 				}
 
 				delete_client(before_client);
-				// delete_client(input->node->sub_client);
-                // zfree(input->node->sub_client->id);
-                // input->node->sub_client->id = NULL;
-                // zfree(input->node->sub_client);
-                // input->node->sub_client = NULL;
-                // client->id = NULL;
-                // client = NULL;
                 break;
             }
         }
@@ -194,7 +231,7 @@ void add_client(struct topic_and_node *input, char *id)
     cli_add = (struct client*)zmalloc(sizeof(struct client));
     cli_add->id = (char*)zmalloc(strlen(id)+1);
     memcpy(cli_add->id, id, strlen(id)+1);
-    puts(cli_add->id);
+    // puts(cli_add->id);
 
     if (input->node->sub_client == NULL) {
         input->node->sub_client = cli_add;
@@ -230,15 +267,8 @@ void search_node(struct db_tree *db, char *topic_data, struct topic_and_node **t
 
     topic_queue = topic_parse(topic_data);
 
-    // char **re = topic_queue;
-    // while (*re) {
-    //     printf("RES: %s\n", *re++);
-    // }
-
-
     while (*topic_queue && node){
         if (strcmp(node->topic, *topic_queue)) {
-		 // puts("node->topic");
             len = node->len;
             fnode = node;
             fnode->state = UNEQUAL;
@@ -268,25 +298,40 @@ void search_node(struct db_tree *db, char *topic_data, struct topic_and_node **t
             node->state = UNEQUAL;
 			(*tan)->topic = topic_queue;
  			(*tan)->node = node; 
-			printf("node state : %d\n", (*tan)->node->state);
-			// puts("2node->topic");
+			debug("node state : %d\n", (*tan)->node->state);
 			return;
         }
     }
 	return;
 }
 
+void *get_client_info(struct db_node *node) 
+{
+	/* TODO */
 
-struct client *search_client(struct db_node *root, char **topic_queue)
+}
+
+void iterate_client(struct clients * sub_clients /*, void func*/) 
+{
+	/* TODO */
+	/* iterator and do */
+
+	/* func(client); */
+}
+
+
+struct clients *search_client(struct db_node *root, char **topic_queue)
 {
     assert(root && topic_queue);
 	size_t client_len = 1; 
-	struct client *res = NULL;
+	struct clients *res = NULL;
+	// res = (struct clients*)zmalloc(sizeof(struct clients));
+	// memset(res, 0, sizeof(struct client));
+	struct clients *tmp_ptr = NULL;
+	tmp_ptr = (struct clients*)zmalloc(sizeof(struct clients));
+	memset(tmp_ptr, 0, sizeof(struct clients));
+	res = tmp_ptr;
     struct db_node *node = root;
-	res = (struct client*)zmalloc(sizeof(struct client));
-	memset(res, 0, sizeof(struct client));
-	struct client *tmp_ptr = NULL;
-	res->next = tmp_ptr;
 
     while (*topic_queue && node){
 		if (strcmp(node->topic, *topic_queue)) {/* if can't ,add '+' judge here */
@@ -303,102 +348,91 @@ struct client *search_client(struct db_node *root, char **topic_queue)
 				return res;
             }
         }
-		// if (node->hashtag) {
-		// 	struct client* sub_client = node->next->sub_client;
-		// 	int len = sub_client->len;
-		// 	client_len += len;
-		// 	tmp_ptr = zrealloc(tmp_ptr, client_len * sizeof(struct client));
-		// 	while (len > 0 && sub_client) {
-		// 		tmp_ptr = sub_client;/* fixed */
-		// 		tmp_ptr = tmp_ptr->next;
-		// 		sub_client = sub_client->next;
-		// 		len--;
-		// 	}
-		// }
 
-		// if (node->plus) { 
-		// 	if (node->down->down && *(topic_queue+2)) {
-		// 		// search_client(node->down, topic_queue+1);
-		// 		tmp_ptr = search_client(node->down->down, topic_queue+2);
-		// 		while (tmp_ptr) {
-		// 			client_len++;
-		// 			tmp_ptr = tmp_ptr->next;
-		// 		}
+		if (node->hashtag) {
+			// struct client* sub_client = node->next->sub_client;
+			// int len = sub_client->len;
+			// client_len += len;
+			tmp_ptr->sub_client = node->next->sub_client;
+			tmp_ptr = tmp_ptr->down;
+			tmp_ptr = (struct clients*)zmalloc(sizeof(struct clients));
+			memset(tmp_ptr, 0, sizeof(struct clients));
+			tmp_ptr = NULL;/* fixed */
 
-        // 	} else if (*(topic_queue+2) == NULL) {
-		// 		struct client* sub_client = node->next->sub_client;
-		// 		int len = sub_client->len;
-		// 		client_len += len;
-		// 		tmp_ptr = zrealloc(tmp_ptr, client_len * sizeof(struct client));
-		// 		while (len > 0 && sub_client) {
-		// 			tmp_ptr = sub_client;/* fixed */
-		// 			tmp_ptr = tmp_ptr->next;
-		// 			sub_client = sub_client->next;
-		// 			len--;
-		// 		}
-        // 	} else if (node->down->down == NULL) { /* + is leaf */
-		// 		/* fixed judge # */
-		// 		int len = 0;
-		// 		if (node->down->next) {
-		// 			node = node->down;
-        //     		len = node->len;
-        //     		while (len > 0 && node->next) {
-        //     		    node = node->next;
-        //     		    if (!strcmp(node->topic, *(topic_queue+1))) {
-        //     		        break;
-        //     		    }
-        //     		    len--;
-        //     		}
-		// 		}
-		// 		if (len == 0) {
-		// 			return res;
-		// 		}
-		// 		if (node->hashtag) {
-		// 			struct client* sub_client = node->next->sub_client;
-		// 			int len = sub_client->len;
-		// 			client_len += len;
-		// 			tmp_ptr = zrealloc(tmp_ptr, client_len * sizeof(struct client));
-		// 			while (len > 0 && sub_client) {
-		// 				tmp_ptr = sub_client;/* fixed */
-		// 				tmp_ptr = tmp_ptr->next;
-		// 				sub_client = sub_client->next;
-		// 				len--;
-		// 			}
+		}
 
-		// 		}
-		// 		return res;
-		// 		} 
-		// } else {
+
+		if (node->plus) { 
+			if (node->down->down && *(topic_queue+2)) {
+				// search_client(node->down, topic_queue+1);
+				tmp_ptr = search_client(node->down->down, topic_queue+2);
+        	} else if (*(topic_queue+2) == NULL) {
+				/* TODO */
+				/* fixed ,to judge if node + client equal NULL or not */
+				tmp_ptr->sub_client = node->next->sub_client;
+				tmp_ptr = tmp_ptr->down;
+				tmp_ptr = (struct clients*)zmalloc(sizeof(struct clients));
+				memset(tmp_ptr, 0, sizeof(struct clients));
+				tmp_ptr = NULL;/* fixed */
+
+				// struct client* sub_client = node->next->sub_client;
+				// int len = sub_client->len;
+				// client_len += len;
+        	} else if (node->down->down == NULL) { /* + is leaf */
+				/* fixed judge # */
+				int len = 0;
+				if (node->down->next) {
+					node = node->down;
+            		len = node->len;
+            		while (len > 0 && node->next) {
+            		    node = node->next;
+            		    if (!strcmp(node->topic, *(topic_queue+1))) {
+            		        break;
+            		    }
+            		    len--;
+            		}
+				}
+
+				if (len == 0) {
+					return res;
+				}
+
+				if (node->hashtag) {
+					// struct client* sub_client = node->next->sub_client;
+					// int len = sub_client->len;
+					// client_len += len;
+					tmp_ptr->sub_client = node->next->sub_client;
+					tmp_ptr = tmp_ptr->down;
+					tmp_ptr = (struct clients*)zmalloc(sizeof(struct clients));
+					memset(tmp_ptr, 0, sizeof(struct clients));
+					tmp_ptr = NULL;/* fixed */
+				}
+
+				return res;
+			}
+		} else {
+
 			if (node->down && *(topic_queue+1)) {
         	    topic_queue++;
         	    node = node->down;
         	} else if (*(topic_queue+1) == NULL) {
-				struct client* sub_client = node->sub_client;
-				printf("sub_client len %ld\n", sub_client->len);
-				int len = sub_client->len;
-				client_len += len+1;
-				printf("client len %ld\n", client_len);
-				tmp_ptr = (struct client*)zmalloc(client_len * sizeof(struct client));
-				// tmp_ptr = zmalloc(tmp_ptr, client_len * sizeof(struct client));
-				while (len+1 > 0 && sub_client) {
-					puts(sub_client->id);
-					tmp_ptr->id = sub_client->id;/* fixed */
-					puts(tmp_ptr->id);
-					tmp_ptr = tmp_ptr->next;
-					sub_client = sub_client->next;
-					len--;
-				}
+				tmp_ptr->sub_client = node->sub_client;
+				tmp_ptr = tmp_ptr->down;
+				tmp_ptr = (struct clients*)zmalloc(sizeof(struct clients));
+				memset(tmp_ptr, 0, sizeof(struct clients));
+				tmp_ptr = NULL;/* fixed */
 
-				while (res->next) { 
-					res = res->next;
-					puts(res->id);
-				}
+
+				// while (res->next) { 
+				// 	res = res->next;
+				// 	puts(res->id);
+				// }
 					
 				return res;
         	} else {
 				return res;
         	}
-		// }
+		}
 	}
 
 	return res;
