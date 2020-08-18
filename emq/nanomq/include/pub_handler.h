f875b0ee89d4f2a302d433e7b73ee98a421a914c
alvin1221 <alvin@emqx.io>
2020-08-18 17:33:46 +0800

    * MODIFY [pub_handler, broker] pub handle logic

--- emq/nanomq/include/pub_handler.h
+++ emq/nanomq/include/pub_handler.h
@@ -6,6 +6,7 @@
 #define NANOMQ_PUB_HANDLER_H
 
 #include <nng/nng.h>
+#include <apps/broker.h>
 #include "nng/protocol/mqtt/mqtt.h"
 
 typedef uint32_t variable_integer;
@@ -102,6 +103,7 @@ struct pub_packet_struct {
 
 };
 
+bool handle_pub(emq_work *work, nng_msg *send_msg, struct topic_and_node *tp_node);
 void pub_handler(void *arg, nng_msg *send_msg);
 bool encode_pub_message(nng_msg *msg, struct pub_packet_struct *pub_packet);
 bool decode_pub_message(nng_msg *msg, struct pub_packet_struct *pub_packet);
